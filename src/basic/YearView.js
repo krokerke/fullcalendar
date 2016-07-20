b659b2e2c1a885c1cce714344d4d7d6c64f826f8
Kevin Kroker <kroker@saltation.de>
2016-07-20 15:25:20 +0200

    adding current dist

---
+++ src/basic/YearView.js
@@ -0,0 +1,23 @@
+
+/* A month view with day cells running in rows (one-per-week) and columns
+----------------------------------------------------------------------------------------------------------------------*/
+
+var YearView = FC.YearView = BasicView.extend({
+
+	// Produces information about what range to display
+	computeRange: function(date) {
+		var range = BasicView.prototype.computeRange.call(this, date); // get value from super-method
+
+		return range;
+	},
+	// Generates the DayGrid object this view needs. Draws from this.dayGridClass
+	instantiateDayGrid: function() {
+		// generate a subclass on the fly with BasicView-specific behavior
+		// TODO: cache this subclass
+		var subclass = this.dayGridClass.extend(basicDayGridMethods);
+
+		return new subclass(this);
+	}
+});
+
+
